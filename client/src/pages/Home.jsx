ad92671b419a1aec49d3c1aff5d983ce11b72f04
trangvan <trang@akocommerce.com>
2023-07-16 13:48:04 +0700

    specify the default values of threejs

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+import state from '../store'
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+ } from '../config/motion'
 
 const Home = () => {
+    const snap = useSnapshot(state)
+
   return (
     <div>Home</div>
   )
