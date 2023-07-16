bf5a1c8816291c2c6c0f0ae368f76f72e07698f6
trangvan <trang@akocommerce.com>
2023-07-16 21:42:15 +0700

    update ai picker

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,20 +1,6 @@
-// import express from 'express';
-// import * as dotenv from 'dotenv';
-// import { Configuration, OpenAIApi } from 'openai'
-
-// dotenv.config()
-
-// const router = express.Router()
-
-// router.route('/').get((req, res) => {
-//     res.status(200).json({ message: "Hello from DALLE ROUTES" })
-// })
-
-// export default router
-
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi} from 'openai';
 
 dotenv.config();
 
@@ -31,24 +17,23 @@ router.route('/').get((req, res) => {
 })
 
 router.route('/').post(async (req, res) => {
-    try {
-      const { prompt } = req.body;
+  try {
+    const { prompt } = req.body;
 
-      const response = await openai.createImage({
-        prompt,
-        n: 1,
-        size: '1024x1024',
-        response_format: 'b64_json'
-      });
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json'
+    });
 
-      const image = response.data.data[0].b64_json;
+    const image = response.data.data[0].b64_json;
 
-      res.status(200).json({ photo: image });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Something went wrong" })
-    }
-  })
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" })
+  }
+})
 
-
-export default router
+export default router;
