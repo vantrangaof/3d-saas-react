9667254a563d80508dc01283e03987a8de434914
trangvan <trang@akocommerce.com>
2023-07-16 20:30:24 +0700

    update filter tab

--- README.md
+++ README.md
@@ -4,10 +4,14 @@ Steps to init project:
 
 - ```npm create vite@latest -- --template react client``` install the packages in the 'client' folder
 - ```npm install three @react-three/fiber @react-three/drei maath valtio react-color framer-motion``` install 3d packages
-- ```npx tailwindcss init -p``` install tailwind packages. Then follow the guide [here](https://tailwindcss.com/docs/guides/vite) to complete the learning
+- ```npx tailwindcss init -p``` install tailwind packages. Then follow the guide [here](https://tailwindcss.com/docs/guides/vite) to complete the Tailwind settings.
+How to install Tailwind in React: [view this article](https://www.freecodecamp.org/news/how-to-install-tailwindcss-in-react/)
+
 - Update the web assets using the [files here](https://drive.google.com/drive/folders/166wA5NsMV_5D8NN7ujDDbPXC1X65vf2I)
 - ES7+ React extension
--> Type `rafce` to init an arrow function
+-> Type `rafce` to init an arrow
+
+- To run the project, cd to the project directory (in this case is the `client` folder)
 
 
 ## Notes:
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-fallthrough */
 /* eslint-disable no-unused-vars */
 import { useState, useEffect } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useSnapshot } from 'valtio'
 
-import config from '../config/config.js'
+import config from '../config/config'
 import state from '../store'
 import { download } from '../assets'
 import { downloadCanvasToImage, reader } from '../config/helpers'
@@ -19,7 +20,7 @@ const Customizer = () => {
     const [activeEditorTab, setActiveEditorTab] = useState("")
     const [activeFilterTab, setActiveFilterTab] = useState({
         logoShirt: true,
-        stylishShirt: false
+        stylishShirt: false,
     })
 
     // show tab content depending on the active tab
@@ -29,12 +30,56 @@ const Customizer = () => {
             case "colorpicker":
                 return <ColorPicker />
             case "filepicker":
-                return <FilePicker />
+                return <FilePicker
+                    file={file}
+                    setFile={setFile}
+                    readFile={readFile}
+                />
             case "aipicker":
                 return <AIPicker />
             default:
                 return null
         }
+    }
+
+    const handleDecals = (type, result) => {
+        const decalType = DecalTypes[type]
+        state[decalType.stateProperty] = result;
+        if (!activeFilterTab[decalType.filterTab]) {
+            handleActiveFilterTab(decalType.filterTab)
+        }
+    }
+
+    const handleActiveFilterTab = (tabName) => {
+        switch (tabName) {
+          case "logoShirt":
+              state.isLogoTexture = !activeFilterTab[tabName];
+            break;
+          case "stylishShirt":
+              state.isFullTexture = !activeFilterTab[tabName];
+            break;
+          default:
+            state.isLogoTexture = true;
+            state.isFullTexture = false;
+            break;
+        }
+
+        // after setting the state, activeFilterTab is updated
+
+        setActiveFilterTab((prevState) => {
+            return {
+            ...prevState,
+            [tabName]: !prevState[tabName]
+            }
+        })
+    }
+
+    const readFile = (type) => {
+        reader(file)
+        .then((result) => {
+            handleDecals(type, result);
+            setActiveEditorTab("")
+        })
     }
 
   return (
@@ -79,8 +124,8 @@ const Customizer = () => {
                             key={tab.name}
                             tab={tab}
                             isFilterTab
-                            isActiveTab=""
-                            handleClick={() => {}}
+                            isActiveTab={activeFilterTab[tab.name]}
+                            handleClick={() => handleActiveFilterTab(tab.name)}
                         />
                     ))}
 
