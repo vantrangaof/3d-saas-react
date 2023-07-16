73064adb119e887172de2d46ff994b6652643c78
trangvan <trang@akocommerce.com>
2023-07-16 14:14:42 +0700

    build homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 import { motion, AnimatePresence } from 'framer-motion'
 import { useSnapshot } from 'valtio'
 import state from '../store'
+import { CustomButton } from '../components'
 import {
     headContainerAnimation,
     headContentAnimation,
@@ -12,7 +14,38 @@ const Home = () => {
     const snap = useSnapshot(state)
 
   return (
-    <div>Home</div>
+    <AnimatePresence>
+        {snap.intro && (
+        <motion.section className="home" {...slideAnimation('left')}>
+            <motion.header {...slideAnimation("down")}>
+                <img
+                src='./threejs.png'
+                alt="logo"
+                className="w-8 h-8 object-contain"
+                />
+            </motion.header>
+            <motion.div className="home-content" {...headContainerAnimation}>
+                <motion.div {...headTextAnimation}>
+                    <h1 className="head-text">
+                        LET'S <br className="xl:block hidden" /> DO IT.
+                    </h1>
+                </motion.div>
+                <motion.div {...headContentAnimation}
+                    className="flex flex-col gap-5"
+                >
+                    <p className="max-w-md font-normal text-gray-600 text-base">
+                        Create your unique and exclusive shirt with our brand new 3D customization tool.<strong> Unleash your imagination</strong>{" "} and define your own style.
+                    </p>
+                </motion.div>
+                <CustomButton
+                    type="filled"
+                    title="Customize it"
+                    handleClick={() => state.intro = false}
+                    customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                />
+            </motion.div>
+        </motion.section>)}
+    </AnimatePresence>
   )
 }
 
