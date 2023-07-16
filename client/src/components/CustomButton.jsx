5a32d1f89be615e09fe866ba3cefcd15dac83e0c
trangvan <trang@akocommerce.com>
2023-07-16 20:47:18 +0700

    complete file picker

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -2,6 +2,8 @@
 import { useSnapshot } from 'valtio'
 import state from '../store'
 
+import { getContrastingColor } from '../config/helpers'
+
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
 
     const snap = useSnapshot(state);
@@ -9,7 +11,13 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
         if(type === 'filled') {
             return {
                 background: snap.color,
-                color: "#fff"
+                color: getContrastingColor(snap.color)
+            }
+        } else if (type === 'outline') {
+            return {
+                borderWidth: '1px',
+                borderColor: snap.color,
+                color: snap.color
             }
         }
     }
